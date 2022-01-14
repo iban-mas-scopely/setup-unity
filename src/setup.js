faf9864a3845ba060e6a2f618b7889c8140e6b97
Iban Mas <62250107+iban-mas-scopely@users.noreply.github.com>
2022-01-14 11:57:52 +0100

    Ignore return code for darwin platform
    With Unity Hub 3.0 update the exit codes for the darwin platform when installing Unity are different to 0 even on success

--- src/setup.js
+++ src/setup.js
@@ -174,7 +174,7 @@ async function executeHub(unityHubPath, args) {
     if (process.platform === 'linux') {
         return await execute(`xvfb-run --auto-servernum "${unityHubPath}" --headless ${args}`);
     } else if (process.platform === 'darwin') {
-        return await execute(`"${unityHubPath}" -- --headless ${args}`);
+        return await execute(`"${unityHubPath}" -- --headless ${args}`, true);
     } else if (process.platform === 'win32') {
         // unityhub always return exit code 1
         return await execute(`"${unityHubPath}" -- --headless ${args}`, true);
